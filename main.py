9a510adee7bc01f7218709dd56ff720db7f23e9e
TaeJuneJoung <jtj0525@gmail.com>
2022-08-18 18:06:34 +0900

    [+]Member Join page

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form_data = request.form
-    user_id = form_data.get('user-id')
-    user_pwd = form_data.get('user-pwd')
-    print('ID:', user_id)
-    print('Password:', user_pwd)
-    return 'Success'
+    if request.method == 'POST':
+        form_data = request.form
+        user_id = form_data.get('user-id')
+        user_pwd = form_data.get('user-pwd')
+        print('ID:', user_id)
+        print('Password:', user_pwd)
+        return 'Success'
+    return render_template('login.html')
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    if request.method == 'POST':
+        form_data = request.form
+        return 'Success'
+    return render_template('join.html')
 
 
 if __name__ == '__main__':
